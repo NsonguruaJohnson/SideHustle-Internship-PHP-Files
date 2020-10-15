3d2b2c2873c771f185de96f005b17c85a12d4470
NsonguruaJohnson <61969839+NsonguruaJohnson@users.noreply.github.com>
2020-10-15 19:06:52 +0100

    My first major task

---
+++ FirstMajorTask.php
@@ -0,0 +1,22 @@
+<?php
+	
+	// === First Major Task ==
+
+	/*
+		Occupation: Recent Graduate
+		Gender: Male
+		Sidehustle Username: Nsongurua
+	*/
+
+	// About me
+	echo "<h1>About Me</h1> <hr>";
+
+	// SideHustle Internship Username
+	echo "<h3>Nsongurua</h3>";
+
+	// More details about me
+	echo "<p>I am a young passionate individual interesting in solving challenges and problems throgh web development. I gradated recently from the University of Uyo, Uyo with a B.Eng. in Electrical/Electronics Engineering.</p>";
+
+
+
+?> 
